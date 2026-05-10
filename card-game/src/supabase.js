d6ae6ac3ef03a517b831298ccba1f9fcfe4e70fb
Takeda731 <takedaronin731@gmail.com>
2026-05-10 18:41:24 +0400

    updat321332

--- card-game/src/supabase.js
+++ card-game/src/supabase.js
@@ -1,11 +1,10 @@
-import { createClient }
-from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
   "https://klnmvjgbllykljfurdwv.supabase.co";
 
 const supabaseKey =
-  "sb_publishable_1LSHWlG3i_kQpLBu4YomMw__ohT7gMI";
+  "ТВОЙ_КЛЮЧ";
 
 export const supabase =
   createClient(
